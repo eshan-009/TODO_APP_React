642884233712799deb34717ad50d2df97837431b
eshan-009 <eshansharma009@gmail.com>
2024-09-22 19:33:32 +0530

    uniqueUser

--- src/services/operations/todos.js
+++ src/services/operations/todos.js
@@ -8,18 +8,24 @@ const {GETTODO} = todoEndPoints
 
 export const getTodo = ()=>{
     return async(dispatch)=>{
+        const token = localStorage.getItem("token")
         try{
-            const toastId = toast.loading("Getting Todos")
-            const response = await apiConnector("GET",GETTODO);
 
+            const toastId = toast.loading("Getting Todos")
+            const response = await apiConnector("GET",GETTODO,{},{
+                "Content-type": "application/json; charset=UTF-8",
+                "Authorization" : `Bearer ${token}`
+            });
+
+            console.log(response.data)
       if(response.status==200)
       {
          dispatch(fetchTodos(response.data.data))
      
-        toast.dismiss(toastId);
+       
       }
-           
-         return response.data.data
+      toast.dismiss(toastId);
+        //  return response.data.data
         }
         catch (err){
             toast.error("Error getting Todos")
